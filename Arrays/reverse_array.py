8c59af6d4b96227ec7ad2a3d73ba874f39b4f370
Atul <notlexius@gmail.com>
2026-06-16 10:59:46 +0530

    Initial commit: Added DSA using Python repository

---
+++ Arrays/reverse_array.py
@@ -0,0 +1,4 @@
+arr = [1, 2, 3, 4, 5]
+
+print("Original Array:", arr)
+print("Reversed Array:", arr[::-1])
---
+++ Arrays/second_largest.py
@@ -0,0 +1,5 @@
+arr = [10, 25, 5, 40, 18]
+
+arr.sort()
+
+print("Second Largest:", arr[-2])
