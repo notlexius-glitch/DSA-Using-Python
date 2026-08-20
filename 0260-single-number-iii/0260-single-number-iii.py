3b32fa42bd93b4164d454bb790bef32fc06ee339
Praval Yadav <notlexius@gmail.com>
2026-08-20 08:07:34 +0530

    Time: 0 ms (100%), Space: 20.6 MB (98.3%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+
+        for num in nums:
+            xor ^= num
+
+        # Find the rightmost bit where the two unique numbers differ
+        diff = xor & -xor
+
+        num1 = 0
+        num2 = 0
+
+        for num in nums:
+            if num & diff:
+                num1 ^= num
+            else:
+                num2 ^= num
+
+        return [num1, num2]
